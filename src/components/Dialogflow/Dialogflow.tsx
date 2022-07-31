3e881a11ccaf935cc85d355162b2ba9406221d10
Jinsung Ha <jsung5381@naver.com>
2022-08-01 01:26:07 +0900

    Add temporal component for dialogflow

---
+++ src/components/Dialogflow/Dialogflow.tsx
@@ -0,0 +1,18 @@
+
+export default function Dialogflow() {
+
+  return (
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    // <df-messenger intent="WELCOME" chat-title="PyMODI QnA Chatbot" chat-icon="https://github.com/LUXROBO/pymodi/blob/gh-pages/assets/img/chat-icon.png?raw=true" agent-id="c8def732-3607-44cb-97a7-805e4f4f6ad8" language-code="en"></df-messenger>
+    // <style>
+    //   df-messenger {
+    //     --df-messenger-bot-message: #6c757d;
+    //     --df-messenger-button-titlebar-color: #49d5e0;
+    //     --df-messenger-chat-background-color: #fafafa;
+    //     --df-messenger-font-color: white;
+    //     --df-messenger-send-icon: #6c757d;
+    //     --df-messenger-user-message: #49d5e0;
+    //   }
+    // </style>
+  );
+}
