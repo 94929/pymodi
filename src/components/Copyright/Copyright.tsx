eba9a957d357976699fa23948f999d8bd83efb53
Jinsung Ha <jsung5381@naver.com>
2022-08-01 01:36:13 +0900

    Add copyright

---
+++ src/components/Copyright/Copyright.tsx
@@ -0,0 +1,9 @@
+
+export default function Copyright() {
+
+  return (
+    <div className="copyright py-4 text-center text-white">
+        <div className="container"><small>Copyright © PyMODI Official 2018</small></div>
+    </div>
+  );
+}
